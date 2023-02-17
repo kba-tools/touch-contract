002b37aa12fb1a9d167686ecd8b44a90776a95aa
440181N-440H4N4N <mobinm.mohanan@gmail.com>
2023-02-17 17:17:21 +0530

    solidity-version_updated

--- bin/cli.js
+++ bin/cli.js
@@ -3,7 +3,7 @@
 import { writeFileSync } from 'fs';
 import { mkdirp } from 'mkdirp';
 
-const contract = '// SPDX-License-Identifier: MIT\npragma solidity 0.8.17;\n\ncontract Cert {\n    address admin;\n\n    constructor() {\n        admin = msg.sender;\n    }\n\n    modifier onlyAdmin() {\n        require(msg.sender == admin, "Access Denied");\n        _;\n    }\n\n    struct Certificate {\n        string course;\n        string name;\n        string grade;\n        string date;\n    }\n\n    mapping(string => Certificate) public Certificates;\n\n    function issue(\n        string memory _id,\n        string memory _course,\n        string memory _name,\n        string memory _grade,\n        string memory _date\n    ) public onlyAdmin {\n        Certificates[_id] = Certificate(_course, _name, _grade, _date);\n    }\n}\n';
+const contract = '// SPDX-License-Identifier: MIT\npragma solidity 0.8.18;\n\ncontract Cert {\n    address admin;\n\n    constructor() {\n        admin = msg.sender;\n    }\n\n    modifier onlyAdmin() {\n        require(msg.sender == admin, "Access Denied");\n        _;\n    }\n\n    struct Certificate {\n        string course;\n        string name;\n        string grade;\n        string date;\n    }\n\n    mapping(string => Certificate) public Certificates;\n\n    function issue(\n        string memory _id,\n        string memory _course,\n        string memory _name,\n        string memory _grade,\n        string memory _date\n    ) public onlyAdmin {\n        Certificates[_id] = Certificate(_course, _name, _grade, _date);\n    }\n}\n';
 
 const touchContract = async () => {
     try {
